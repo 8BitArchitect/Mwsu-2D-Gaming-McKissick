8753957deb17fa93ae8b5ce521ec7fcebd78add4
8BitArchitect <iamnobody@sbcglobal.net>
2016-06-09 20:26:11 -0400

    modified walls and game size

--- Program_1/main.js
+++ Program_1/main.js
@@ -2,8 +2,7 @@ var mainState = {
 
     preload: function() {
         game.load.image('player', 'assets/player.png');
-        game.load.image('wallV', 'assets/wallVertical.png');
-        game.load.image('wallH', 'assets/wallHorizontal.png');
+        game.load.image('wall', 'assets/wall.png');
         game.load.image('coin', 'assets/coin.png');
         game.load.image('enemy', 'assets/enemy.png');
     },
@@ -17,6 +16,7 @@ var mainState = {
         
         this.player = game.add.sprite(game.width/2, game.height/2, 'player');
         this.player.anchor.setTo(0.5, 0.5);
+		//this.player.scale.setTo(2, 2)
         game.physics.arcade.enable(this.player);
         this.player.body.gravity.y = 1000;
 
@@ -25,6 +25,7 @@ var mainState = {
         this.coin = game.add.sprite(60, 140, 'coin');
         game.physics.arcade.enable(this.coin); 
         this.coin.anchor.setTo(0.5, 0.5);
+		//this.coin.scale.setTo(2, 2)
 
         this.scoreLabel = game.add.text(30, 30, 'score: 0', { font: '18px Arial', fill: '#ffffff' });
         this.score = 0;
@@ -102,26 +103,32 @@ var mainState = {
         enemy.body.bounce.x = 1;
         enemy.checkWorldBounds = true;
         enemy.outOfBoundsKill = true;
+		//enemy.scale.setTo(2, 2);
     },
 
     createWorld: function() {
         this.walls = game.add.group();
         this.walls.enableBody = true;
+		// Here you can apply the same properties to every wall.
+		// http://stackoverflow.com/a/29505980
+		this.walls.setAll('anchor.x', 0.5);
+		this.walls.setAll('anchor.y', 0);	
 
-        game.add.sprite(0, 0, 'wallV', 0, this.walls); 
-        game.add.sprite(480, 0, 'wallV', 0, this.walls); 
-        game.add.sprite(0, 0, 'wallH', 0, this.walls); 
-        game.add.sprite(300, 0, 'wallH', 0, this.walls);
-        game.add.sprite(0, 320, 'wallH', 0, this.walls); 
-        game.add.sprite(300, 320, 'wallH', 0, this.walls); 
-        game.add.sprite(-100, 160, 'wallH', 0, this.walls); 
-        game.add.sprite(400, 160, 'wallH', 0, this.walls); 
-        var middleTop = game.add.sprite(100, 80, 'wallH', 0, this.walls);
+        game.add.sprite(10, 0, 'wallV', 0, this.walls); 
+        game.add.sprite(game.width-10, 0, 'wallV', 0, this.walls); 
+        game.add.sprite(10, 0, 'wallH', 0, this.walls); 
+        game.add.sprite(game.width*.75, 0, 'wallH', 0, this.walls);
+        game.add.sprite(0, game.height-20, 'wallH', 0, this.walls); 
+        game.add.sprite(game.width-20, game.height-20, 'wallH', 0, this.walls); 
+        game.add.sprite(game.width*.25, game.height/2-10, 'wallH', 0, this.walls); 
+        game.add.sprite(game.width*.75, game.height/2-10, 'wallH', 0, this.walls); 
+        var middleTop = game.add.sprite(game.width/2, game.height*.25, 'wallH', 0, this.walls);
         middleTop.scale.setTo(1.5, 1);
-        var middleBottom = game.add.sprite(100, 240, 'wallH', 0, this.walls);
+        var middleBottom = game.add.sprite(game.width/2, game.height*.75, 'wallH', 0, this.walls);
         middleBottom.scale.setTo(1.5, 1);
 
         this.walls.setAll('body.immovable', true);
+		console.log(this.walls.children)
     },
 
     playerDie: function() {
@@ -129,6 +136,6 @@ var mainState = {
     },
 };
 
-var game = new Phaser.Game(500, 340, Phaser.AUTO, 'gameDiv');
+var game = new Phaser.Game(640, 480, Phaser.AUTO, 'gameDiv');
 game.state.add('main', mainState);
 game.state.start('main');
