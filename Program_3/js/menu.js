e3d1a8d5378c3eb9cdba602f079a56b9949fa79c
8BitArchitect <iamnobody@sbcglobal.net>
2016-06-16 06:14:28 -0400

    added program 3

---
+++ Program_3/js/menu.js
@@ -0,0 +1,93 @@
+var menuState = {
+
+    create: function() { 
+        game.add.image(0, 0, 'background');
+		
+		game.global.menuMusic.fadeTo(1000, .5);
+		game.global.gameMusic.fadeTo(2000, .01);
+
+        // Changed the y position to -50 so we don't see the label
+		var nameLabel = game.add.text(game.width/2, -50, 'Super Coin Box', { font: '50px Arial', fill: '#ffffff' });
+		nameLabel.anchor.setTo(.5, .5);
+		
+		// Create a tween on the label
+		var tween = game.add.tween(nameLabel).to({y: 80}, 1000).easing(Phaser.Easing.Bounce.Out).start();
+
+		// If 'bestScore' is not defined
+		// It means that this is the first time the game is played
+		if (!localStorage.getItem('bestScore')) {
+			// Then set the best score to 0
+			localStorage.setItem('bestScore', 0);
+		}
+		// If the score is higher than the best score
+		if (game.global.score > localStorage.getItem('bestScore')) {
+			// Then update the best score
+			localStorage.setItem('bestScore', game.global.score);
+		}
+		var text = 'score: ' + game.global.score + '\nbest score: ' + localStorage.getItem('bestScore');
+		var scoreLabel = game.add.text(game.width/2, game.height/2, text, { font: '25px Arial', fill: '#ffffff', align: 'center' });
+		scoreLabel.anchor.setTo(0.5, 0.5);
+
+        var startLabel = game.add.text(game.width/2, game.height-80, 'press the up arrow key to start', { font: '25px Arial', fill: '#ffffff' });
+        startLabel.anchor.setTo(0.5, 0.5);
+		
+		// Add the button that calls the 'toggleSound' function when pressed
+		this.muteButton = game.add.button(20, 20, 'mute', this.toggleSound,this);
+		
+		// Create the tween
+		tween = game.add.tween(startLabel);
+		// Rotate the label to -2 degrees in 500ms
+		tween.to({angle: -2}, 500);
+		// Then rotate the label to +2 degrees in 1000ms
+		tween.to({angle: 2}, 1000);
+		// And get back to our initial position in 500ms
+		tween.to({angle: 0}, 500);
+		// Loop indefinitely the tween
+		tween.loop();
+		// Start the tween
+		tween.start();
+
+        var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+        upKey.onDown.add(this.start, this);
+    },
+	
+	update: function() {
+		if (!game.global.gameMusic.isPlaying) {
+			game.global.gameMusic.play();
+			game.global.menuMusic.play();
+		}
+		this.syncMusic(game.global.gameMusic, game.global.menuMusic);
+	},
+	
+	syncMusic: function(song1, song2) {
+		if (game.math.fuzzyGreaterThan(song1.curentTime, song2.currentTime, 50))
+		{
+			song1.pause();
+		}
+		else if (game.math.fuzzyGreaterThan(song2.curentTime, song1.currentTime, 50))
+		{
+			song2.pause();
+		}
+		if (!song1.isPlaying && game.math.fuzzyEqual(song2.curentTime, song1.currentTime, 50))
+		{
+			song1.play();
+		}
+		if (!song2.isPlaying && game.math.fuzzyEqual(song2.curentTime, song1.currentTime, 50))
+		{
+			song2.play();
+		}
+	},
+	
+	// Function called when the 'muteButton' is pressed
+	toggleSound: function() {
+		// Switch the variable from true to false, or false to true
+		// When 'game.sound.mute = true', Phaser will mute the game
+		game.sound.mute = !game.sound.mute;
+		// Change the frame of the button
+		this.muteButton.frame = game.sound.mute ? 1 : 0;
+	},
+
+    start: function() {
+        game.state.start('play');   
+    },
+};
---
+++ Program_3/js/play.js
@@ -0,0 +1,443 @@
+var playState = {
+
+    create: function() { 
+		game.global.menuMusic.fadeTo(2000, .01);
+		game.global.gameMusic.fadeTo(1000, .5);
+        this.cursor = game.input.keyboard.createCursorKeys();
+        
+		//set default properties for the player
+        this.player = game.add.sprite(game.width/2, game.height/2, 'player');
+		this.player.animations.add('none', [0], 0, true);
+		this.player.animations.add('right', [1], 0, true);
+		this.player.animations.add('left', [4], 0, true);
+		this.player.animations.add('walkRight', [1, 2, 1, 3], 8, true);
+		this.player.animations.add('walkLeft', [4, 5, 4, 6], 8, true);
+		this.player.animations.add('jumpUp', [11], 0, true);
+		this.player.animations.add('jumpRight', [9], 0, true);
+		this.player.animations.add('jumpLeft', [7], 0, true);
+		this.player.animations.add('fallDown', [12], 0, true);
+		this.player.animations.add('fallRight', [10], 0, true);
+		this.player.animations.add('fallLeft', [8], 0, true);
+		this.playerFacing = 'none';
+        this.player.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(this.player);
+        this.player.body.gravity.y = 1000;
+		this.player.body.bounce.y = .1;
+		this.player.body.bounce.x = .1;
+		this.player.body.drag.x = 1000;
+		this.player.body.setSize(12, 32);
+		
+		this.jumpSound = game.add.audio('jump');
+		this.jumpSound.volume = .33;
+		this.jumpSound.allowMultiple = true;
+		this.coinSound = game.add.audio('coin');
+		this.coinSound.volume = .33;
+		this.deadSound = game.add.audio('dead');
+		this.deadSound.volume = .33;
+
+        this.createWorld();
+
+		//set default properties for the 'coin'
+		this.coinWeights = [5, 4, 3, 2, 1]
+		this.coinValues = [1, 3, 5, 10, 25];
+        this.coin = game.add.sprite(-40, -40, 'ticket');
+		this.updateCoinPosition();
+        game.physics.arcade.enable(this.coin); 
+        this.coin.anchor.setTo(0.5, 0.5);
+
+		//add labels
+        this.scoreLabel = game.add.text(30, 30, 'score: 0', { font: '18px Arial', fill: '#ffffff' });
+        game.global.score = 0;
+		
+		this.deathsLabel = game.add.text(530, 430, 'deaths: 0', { font: '18px Arial', fill: '#ffffff' });
+        this.deaths = 0;
+		
+		this.timeLabel = game.add.text(420, 30, 'time left: 120', { font: '18px Arial', fill: '#ffffff' });
+		this.startTime = game.time.now;
+		this.timeLeft = 600;
+		
+		//vars for player invuln state
+		this.playerInvuln = false;
+		this.invulnTil = 0;
+		this.pauseTil = 0;
+
+		//initialize enemies
+        this.enemies = game.add.group();
+        this.enemies.enableBody = true;
+        this.enemies.createMultiple(10, 'enemy');
+        game.time.events.loop(3600, this.addEnemy, this);
+		
+		// Create the emitter with 15 particles. We don't need to set the x y
+		// Since we don't know where to do the explosion yet
+		this.emitter = game.add.emitter(0, 0, 15);
+
+		// Set the 'pixel' image for the particles
+		this.emitter.makeParticles('pixel');
+
+		// Set the x and y speed of the particles between -150 and 150
+		// Speed will be randomly picked between -150 and 150 for each particle
+		this.emitter.setYSpeed(-150, 150);
+		this.emitter.setXSpeed(-150, 150);
+
+		// Scale the particles from 2 time their size to 0 in 800ms
+		// Parameters are: startX, endX, startY, endY, duration
+		this.emitter.setScale(2, 0, 2, 0, 800);
+
+		// Use no gravity
+		this.emitter.gravity = 0;
+    },
+
+    update: function() {
+		
+		//set which objects/groups can collide with each other
+        game.physics.arcade.collide(this.player, this.walls);
+        game.physics.arcade.collide(this.enemies, this.walls);
+		game.physics.arcade.collide(this.enemies, this.enemies);
+        game.physics.arcade.overlap(this.player, this.coin, this.takeCoin, null, this);
+        game.physics.arcade.overlap(this.player, this.enemies, this.playerDie, null, this);
+		
+		//check if player invulnerability should be reset
+		if (this.playerInvuln && game.time.now > this.invulnTil)
+		{
+			this.player.tint = 0xFFFFFF;
+			this.playerInvuln = false;
+		}
+		
+		if (game.time.now > this.pauseTil)
+		{
+			game.physics.arcade.isPaused = false;
+		}
+
+        this.movePlayer();
+		this.animatePlayer();
+
+		//check if player has left the world
+        if (!this.player.inWorld) {
+            this.playerDie(this.player);
+        }
+		
+		//Enemy AI
+		this.enemies.forEachAlive(this.enemyAI, this, this.player);
+		this.enemies.forEachAlive(this.enemyAnimate, this)
+		
+		//update game timer
+		this.timeLeft=(600-Math.floor((game.time.now-this.startTime)/1000));
+		this.timeLabel.text = 'time left: ' + this.timeLeft;
+		if (this.timeLeft<0)
+		{
+			game.state.start('menu');
+		}
+    },
+
+    movePlayer: function() {
+		//move player left
+        if (this.cursor.left.isDown && this.player.body.velocity.x > -360) {
+            this.player.body.velocity.x = Math.max(-360,this.player.body.velocity.x-60);
+			this.playerFacing = 'left';
+			console.log(this.cursor.left.duration)
+        }
+		//move player right
+        else if (this.cursor.right.isDown && this.player.body.velocity.x < 360) {
+            this.player.body.velocity.x = Math.min(360,this.player.body.velocity.x+60);
+			this.playerFacing = 'right';
+			console.log(this.cursor.right.duration)
+        }
+		//make player jump
+        if (this.cursor.up.isDown && this.player.body.touching.down) {
+			if (game.physics.arcade.isPaused == false)
+			{
+				this.jumpSound.play();
+			}
+            this.player.body.velocity.y = -560;
+        }      
+    },
+	
+	animatePlayer: function() {
+		if (this.player.body.touching.down)
+		{
+			if (this.cursor.left.isDown)
+			{
+				this.player.animations.play('walkLeft');
+			}
+			else if (this.cursor.right.isDown)
+			{
+				this.player.animations.play('walkRight');
+			}
+			else
+			{
+				this.player.animations.play(this.playerFacing);
+			}
+		}
+		else
+		{
+			if (this.playerFacing == 'left')
+			{
+				if (this.player.body.velocity.y < 0)
+				{
+					this.player.animations.play('jumpLeft');
+				}
+				else
+				{
+					this.player.animations.play('fallLeft');
+				}
+			}
+			else if (this.playerFacing == 'right')
+			{
+				if (this.player.body.velocity.y < 0)
+				{
+					this.player.animations.play('jumpRight');
+				}
+				else
+				{
+					this.player.animations.play('fallRight');
+				}
+			}
+			else
+			{
+				if (this.player.body.velocity.y < 0)
+				{
+					this.player.animations.play('jumpUp');
+				}
+				else
+				{
+					this.player.animations.play('fallDown');
+				}
+			}
+		}
+	},
+	
+	enemyAI: function(enemy, player) {
+		if (game.physics.arcade.distanceBetween(enemy, player) < 96 && this.playerInvuln == false)
+		{
+			enemy.body.velocity.x = Math.abs(enemy.body.velocity.x) * game.math.sign(player.body.x-enemy.body.x);
+			if (player.body.y < enemy.body.y && enemy.body.touching.down)
+			{
+				enemy.body.velocity.y = -360;
+				if (game.physics.arcade.isPaused == false)
+				{
+					this.jumpSound.play();
+				}
+			}
+		}	
+	},
+	
+	enemyAnimate(enemy)
+	{
+		if (enemy.body.touching.down && game.physics.arcade.isPaused == false)
+		{
+			enemy.animations.play('move');
+		}
+		else if (enemy.body.velocity.y <= -1)
+		{
+			enemy.animations.play('jump');
+		}
+		else
+		{
+			enemy.animations.play('fall');
+		}
+	},
+
+    takeCoin: function(player, coin) {
+		this.coinSound.play();
+		// Scale the coin to 0 to make it invisible
+		this.coin.scale.setTo(0, 0);
+		// Grow the coin back to its original scale in 300ms
+		game.add.tween(this.coin.scale).to({x: 1, y: 1}, 300).start();
+		game.add.tween(this.player.scale).to({x: 1.3, y: 1.3}, 100).yoyo(true).start();
+        game.global.score += this.coin.value;
+        this.scoreLabel.text = 'score: ' + game.global.score;
+
+        this.updateCoinPosition();
+    },
+
+    updateCoinPosition: function() {
+		//list of possible coin positions
+        var coinPosition = [
+			{x: 160, y:  60}, {x: 480, y:  60},
+			{x: 160, y: 180}, {x: 480, y: 180},
+			{x: 90,  y: 120}, {x: 550, y: 120},
+			{x: 160, y: 300}, {x: 480, y: 300},
+			{x: 160, y: 420}, {x: 480, y: 420},
+			{x: 90,  y: 360}, {x: 550, y: 360}, 
+            {x: 220, y: 240}, {x: 420, y: 240},
+            {x: 320, y: 420}, {x: 320, y:  80}
+        ];
+		
+		var sum = [0,0,0,0,0];
+		sum[0] = this.coinWeights[0];
+		for(i = 1; i < 5; i++)
+		{
+			sum[i] = sum[i-1] + this.coinWeights[i]
+		}
+		var coinRnd = game.rnd.integerInRange(0, sum[4]-1);
+		if (coinRnd < sum[0] && sum[0])
+		{
+			this.coinWeights[0] -= 5;
+			this.coinWeights[1] += 4;
+			this.coinWeights[2] += 3;
+			this.coinWeights[3] += 2;
+			this.coinWeights[4] += 1;
+			this.coin.frame = 0;
+		}
+		else if (coinRnd < sum[1] && sum[1])
+		{
+			this.coinWeights[0] += 5;
+			this.coinWeights[1] -= 4;
+			this.coinWeights[2] += 3;
+			this.coinWeights[3] += 2;
+			this.coinWeights[4] += 1;
+			this.coin.frame = 1;
+		}
+		else if (coinRnd < sum[2] && sum[2])
+		{
+			this.coinWeights[0] += 5;
+			this.coinWeights[1] += 4;
+			this.coinWeights[2] -= 3;
+			this.coinWeights[3] += 2;
+			this.coinWeights[4] += 1;
+			this.coin.frame = 2;
+		}
+		else if (coinRnd < sum[3] && sum[3])
+		{
+			this.coinWeights[0] += 5;
+			this.coinWeights[1] += 4;
+			this.coinWeights[2] += 3;
+			this.coinWeights[3] -= 2;
+			this.coinWeights[4] += 1;
+			this.coin.frame = 3;
+		}
+		else
+		{
+			this.coinWeights[0] += 5;
+			this.coinWeights[1] += 4;
+			this.coinWeights[2] += 3;
+			this.coinWeights[3] += 2;
+			this.coinWeights[4] -= 1;
+			this.coin.frame = 4;
+		}
+		console.log(this.coinWeights);
+		this.coin.value = this.coinValues[this.coin.frame];
+
+		//??? removes current coin position from list ???
+        for (var i = 0; i < coinPosition.length; i++) {
+            if (coinPosition[i].x == this.coin.x && coinPosition[i].y == this.coin.y) {
+                coinPosition.splice(i, 1);
+            }
+        }
+
+        var newPosition = game.rnd.pick(coinPosition);
+        this.coin.reset(newPosition.x, newPosition.y);
+    },
+
+    addEnemy: function() {
+		if (game.physics.arcade.isPaused == false)
+		{
+			//picks the first dead enemy to spawn
+			var enemy = this.enemies.getFirstDead();
+
+			//return if there are no dead enemies 
+			if (!enemy) {
+				return;
+			}
+
+			//set default properties for enemy
+			enemy.animations.add('move', [0, 1, 0, 2], 8, true);
+			enemy.animations.add('jump', [3], 0, true);
+			enemy.animations.add('fall', [4], 0, true);
+			enemy.animations.play('move');
+			enemy.anchor.setTo(0.5, 0.5);
+			enemy.reset(game.width/2, 0);
+			enemy.body.gravity.y = 1000;
+			enemy.body.velocity.x = game.rnd.pick([100, 150, 200,]) * game.rnd.pick([-1, 1]);
+			enemy.body.bounce.x = 1;
+			enemy.body.bounce.y = .5;
+			enemy.checkWorldBounds = true;
+			enemy.outOfBoundsKill = true;
+		}
+    },
+
+    createWorld: function() {
+		//initialize and add the walls to the world
+        this.walls = game.add.group();
+        this.walls.enableBody = true;
+		
+		var left = game.add.sprite(0, 0, 'wall', 0, this.walls);
+		left.scale.setTo(1, 24);
+        var right = game.add.sprite(game.width-20, 0, 'wall', 0, this.walls);
+		right.scale.setTo(1, 24);
+        var topLeft = game.add.sprite(0, 0, 'wall', 0, this.walls);
+		topLeft.scale.setTo(12, 1);
+        var topRight = game.add.sprite(game.width-240, 0, 'wall', 0, this.walls);
+		topRight.scale.setTo(12, 1);
+        var bottomLeft = game.add.sprite(0, game.height-20, 'wall', 0, this.walls);
+		bottomLeft.scale.setTo(12, 1);
+        var bottomRight = game.add.sprite(game.width-240, game.height-20, 'wall', 0, this.walls);
+		bottomRight.scale.setTo(12, 1);
+        var middleLeft = game.add.sprite(0, game.height/2-10, 'wall', 0, this.walls);
+		middleLeft.scale.setTo(8, 1);
+        var middleRight = game.add.sprite(game.width*.75, game.height/2-10, 'wall', 0, this.walls);
+		middleRight.scale.setTo(8, 1);
+        var middleTop = game.add.sprite(game.width*.25, game.height*.25-10, 'wall', 0, this.walls);
+        middleTop.scale.setTo(16, 1);
+        var middleBottom = game.add.sprite(game.width*.25, game.height*.75-10, 'wall', 0, this.walls);
+        middleBottom.scale.setTo(16, 1);
+
+		//prevent walls from moving in collisions
+        this.walls.setAll('body.immovable', true);
+    },
+
+    playerDie: function(player, enemy) {
+		var playSound = false;
+		if(this.playerInvuln == false || player.inWorld == false)
+		{
+			//if the player fell out of the world ignore this section
+			if (enemy)
+			{
+				//throw the player away from the enemy as if it exploded and kill the enemy
+				/* var angle = game.math.angleBetweenPointsY(player.body.center, enemy.body.center);
+				player.body.velocity.x -= Math.sin(angle) * 1080;
+				player.body.velocity.y -= Math.cos(angle) * 1080; */
+				//enemy.animations.play('die');
+				//enemy.kill();
+				//enemy.destroy();
+			}
+			this.pauseTil = game.time.now + 1500
+			game.physics.arcade.isPaused = true;
+			//set player invulnerability
+			this.playerInvuln = true;
+			this.invulnTil = game.time.now + 4500;
+			player.tint = 0xFF0000;
+			
+			//add a death and decrement score (to a min of 0)
+			this.deaths++;
+			this.deathsLabel.text = 'deaths: ' + this.deaths;
+			game.global.score = Math.max(0,game.global.score - 10);
+			this.scoreLabel.text = 'score: ' + game.global.score;
+			playSound = true;
+			var inWall = false;
+			do
+			{
+				inWall = false
+				this.player.reset(game.rnd.integerInRange(40,600),game.rnd.integerInRange(40,440));
+				//make sure we didn't spawn the player in a wall
+				if (
+				((player.body.center.y > 200 && player.body.center.y < 280) && (player.body.center.x < 180 || player.body.center.x > 460)) || 
+				((player.body.center.x > 140 && player.body.center.x < 500) && ((player.body.center.y > 80 && player.body.center.y < 160) || (player.body.center.y > 320 && player.body.center.y < 400)))
+				) 
+				{
+					inWall = true;
+				}
+			}while (inWall);
+			playSound = true;
+			if(playSound)
+			{
+				// Set the position of the emitter on top of the player
+				this.emitter.x = this.player.x;
+				this.emitter.y = this.player.y;
+				// Start the emitter by exploding 15 particles that will live 800ms
+				this.emitter.start(true, 2800, null, 15);
+				this.deadSound.play();
+			}
+		}
+    },
+};
